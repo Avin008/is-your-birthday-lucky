b08fb050f20faa2a00a47ed0b733e78839104767
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-01-11 12:25:08 +0530

    updated es5 code to es6+ syntax

--- script.js
+++ script.js
@@ -3,28 +3,56 @@ const luckyNum = document.querySelector("#lucky-num");
 const checkBtn = document.querySelector("#check-btn");
 const outputBox = document.querySelector("#output-box");
 
-checkBtn.addEventListener("click", checkBirthDayLucky);
+// ES5 CODE
+// *********************************************************************
 
-function calculateSum() {
-  let birthDay = birthDate.value;
-  let dob = birthDay.replaceAll("-", "");
+// checkBtn.addEventListener("click", isBirthdayLucky);
+
+// function calculateSum() {
+//   let birthDay = birthDate.value;
+//   let dob = birthDay.replaceAll("-", "");
+//   let sum = 0;
+//   for (let x = 0; x < dob.length; x++) {
+//     sum += Number(dob[x]);
+//   }
+//   return sum;
+// }
+
+// function checkBirthDayLucky() {
+//   let lucky_Num = Number(luckyNum.value);
+//   let sum = calculateSum();
+//   if (lucky_Num && sum) {
+//     if (sum % lucky_Num === 0) {
+//       outputBox.innerText = "Hurray!! Your BirthDay is Lucky 🎉";
+//     } else {
+//       outputBox.innerText = "Sorry!! Your Birthday is not so Lucky 😔";
+//     }
+//   } else {
+//     outputBox.innerText = "Birth Date and Lucky Number fields cannot be empty";
+//   }
+// }
+
+// *********************************************************************
+
+//ES6+ CODE
+
+const calculateSum = () => {
+  let dob = birthDate.value.replaceAll("-", "");
   let sum = 0;
-  for (let x = 0; x < dob.length; x++) {
-    sum += Number(dob[x]);
+  for (let x of dob) {
+    sum += Number(x);
   }
   return sum;
-}
+};
 
-function checkBirthDayLucky() {
+const isBirthdayLucky = () => {
   let lucky_Num = Number(luckyNum.value);
-  let sum = calculateSum();
-  if (lucky_Num && sum) {
-    if (sum % lucky_Num === 0) {
-      outputBox.innerText = "Hurray!! Your BirthDay is Lucky 🎉";
-    } else {
-      outputBox.innerText = "Sorry!! Your Birthday is not so Lucky 😔";
-    }
-  } else {
-    outputBox.innerText = "Birth Date and Lucky Number fields cannot be empty";
-  }
-}
+  !(lucky_Num && calculateSum())
+    ? (outputBox.innerText =
+        "Birth Date and Lucky Number fields cannot be empty")
+    : calculateSum() % lucky_Num === 0
+    ? (outputBox.innerText = "Hurray!! Your BirthDay is Lucky 🎉")
+    : (outputBox.innerText = "Sorry!! Your Birthday is not so Lucky 😔");
+};
+
+checkBtn.addEventListener("click", isBirthdayLucky);
